1de8dafbee1b1ae4b379b2731d36fde5049bdb3f
VictorCT20 <112089443+VictorCT20@users.noreply.github.com>
2023-10-30 16:31:56 -0500

    f

--- core/settings.py
+++ core/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/sstatic/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 if not DEBUG:
