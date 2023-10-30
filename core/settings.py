df7a3995ac34aee0cc64c73b704e3ec953e67fd4
VictorCT20 <112089443+VictorCT20@users.noreply.github.com>
2023-10-30 18:16:29 -0500

    prueba4.4

--- core/settings.py
+++ core/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = ["templates\static"]
 
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
