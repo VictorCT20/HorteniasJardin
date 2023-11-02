21fdc971fba8b00062afbf07b4ad031ce1d5e206
VictorCT20 <112089443+VictorCT20@users.noreply.github.com>
2023-11-02 17:17:21 -0500

    prueba en bd de render

--- core/settings.py
+++ core/settings.py
@@ -70,13 +70,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
-
 
 
 # Password validation
