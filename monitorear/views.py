62bcf1008d580756e7cb8890b47fa9f9a7aa8023
VictorCT20 <112089443+VictorCT20@users.noreply.github.com>
2023-11-02 01:30:37 -0500

    correccion de la importacion del Usuario

--- monitorear/views.py
+++ monitorear/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View
-from .models import Usuario
+from monitoreo.models import Usuario
 # Create your views here.
 
 class UserEntryView(View):
