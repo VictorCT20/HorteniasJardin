a6b82720fb5c9de0a2205f37b32737774b52bedd
VictorCT20 <112089443+VictorCT20@users.noreply.github.com>
2023-11-01 19:23:59 -0500

    s1

--- Monitoreo/models.py
+++ Monitoreo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    user=models.CharField(max_length=250)
+    contra=models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"Usuario: {self.user}, Contraseña: [Oculto por seguridad]"
