9fb0b57d6da93fd04197fe50e4a960fccc8405e3
VictorCT20 <112089443+VictorCT20@users.noreply.github.com>
2023-11-28 21:21:57 -0500

    debug true

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', default='$h_rz#-1fwthekcjmmgakuh^7dr8nph)6od60ok8)3dl')
 
-DEBUG = False
+DEBUG = True
 # DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
